858c346e506a0a11159ebaf5a5eb95d0f6ccbaeb
yardenivry <107366071+yardenivry@users.noreply.github.com>
2022-12-11 00:40:19 +0000

    almost!

--- index.php
+++ index.php
@@ -8,6 +8,10 @@ $conn = new mysqli($host,$username,$pw);
 
 if ($conn->connect_errno > 0) {
     echo $db->connect_error;
-} else {
-    echo "DB Connection successful\n\n";
+} else { 
+    echo "DB Connection successful\n";
+    $result=mysqli_query($conn,"SHOW DATABASES;");
+    while ($row = mysqli_fetch_row( $result ) ){
+        echo $row[0]."\n";
+    }
 }
